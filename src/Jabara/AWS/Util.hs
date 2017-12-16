eb80beea90d9e0323bf52854871ee57f1d7e130d
jabaraster <ah@jabara.info>
2017-12-17 00:55:24 +0900

    mainte.

--- src/Jabara/AWS/Util.hs
+++ src/Jabara/AWS/Util.hs
@@ -1,56 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell   #-}
 module Jabara.AWS.Util (
-    AWSProfile(..)
-  , AWSProfileName
-  , apAccessKey
-  , apSecretAccessKey
-
-  , readLocalAWSProfile
-  , readLocalAWSProfile'
+    AWSProfileName
+  , discoverCredentials
+  , readCredentials
 ) where
 
-import           Control.Lens
-import           Control.Lens.TH
-import           Data.Ini
+import           Control.Exception
 import           Data.Text
+import           Network.AWS
 import           System.Directory
 
-readLocalAWSProfile :: AWSProfileName -> IO (Either String AWSProfile)
-readLocalAWSProfile profileName = do
-    ini <- getHomeDirectory
-             >>= \h -> readIniFile (h ++ "/.aws/credentials")
-             >>= \i ->
-                     case i of
-                         Left s    -> error s
-                         Right ini -> pure ini
-    pure $ core profileName ini
+discoverCredentials :: AWSProfileName -> IO Credentials
+discoverCredentials profileName = do
+    catch (readCredentials profileName) $
+          \(SomeException e) -> pure $ FromEnv "ENV_AWS_ACCESS_KEY" "ENV_AWS_SECRET_ACCESS_KEY" Nothing Nothing
 
-readLocalAWSProfile' :: FilePath -> AWSProfileName -> IO (Either String AWSProfile)
-readLocalAWSProfile' credentialsFilePath profileName = do
-    ini <- readIniFile credentialsFilePath
-             >>= \i ->
-                     case i of
-                         Left s    -> error s
-                         Right ini -> pure ini
-    pure $ core profileName ini
-
-core :: AWSProfileName -> Ini -> Either String AWSProfile
-core profileName ini =
-    let key = lookupValue profileName "aws_access_key_id" ini
-        sec = lookupValue profileName "aws_secret_access_key" ini
-    in case (key, sec) of
-        (   Left ks,   Left ss ) -> Left (ks ++ "/" ++ ss)
-        (l@(Left s),   _       ) -> Left s
-        (   _      , l@(Left s)) -> Left s
-        (  Right k ,   Right s ) -> Right $ AWSProfile {_apAccessKey = k, _apSecretAccessKey = s}
+readCredentials :: AWSProfileName -> IO Credentials
+readCredentials profileName = do
+    home <- getHomeDirectory
+    pure $ FromFile profileName (home ++ "/.aws/credentials")
 
 type AWSProfileName = Text
-
-data AWSProfile
-    = AWSProfile
-      { _apAccessKey       :: Text
-      , _apSecretAccessKey :: Text
-      }
-    deriving (Show, Read, Eq)
-makeLenses ''AWSProfile
