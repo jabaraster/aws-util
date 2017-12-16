a81ed7f359febcd4503cee44f134af44b489e014
jabaraster <ah@jabara.info>
2017-12-17 01:12:56 +0900

    mainte.

--- src/Jabara/AWS/Util.hs
+++ src/Jabara/AWS/Util.hs
@@ -2,14 +2,28 @@
 {-# LANGUAGE TemplateHaskell   #-}
 module Jabara.AWS.Util (
     AWSProfileName
+
   , discoverCredentials
+  , discoverEnv
   , readCredentials
+
+  , localDynamoDBEnv
 ) where
 
 import           Control.Exception
+import           Control.Lens
+import           Data.Monoid
 import           Data.Text
 import           Network.AWS
+import           Network.AWS.Auth
 import           System.Directory
+
+type AWSProfileName = Text
+
+discoverEnv :: AWSProfileName -> Region -> IO Env
+discoverEnv profileName region = do
+    env <- newEnv =<< discoverCredentials profileName
+    pure $ env&envRegion .~ region
 
 discoverCredentials :: AWSProfileName -> IO Credentials
 discoverCredentials profileName = do
@@ -21,4 +35,21 @@ readCredentials profileName = do
     home <- getHomeDirectory
     pure $ FromFile profileName (home ++ "/.aws/credentials")
 
-type AWSProfileName = Text
+localDynamoDBEnv :: IO Env
+localDynamoDBEnv = do
+    env <- newEnv $ FromEnv "" "" Nothing Nothing
+    pure $ env&envOverride .~ localOverride
+  where
+    localOverride :: Dual (Endo Service)
+    localOverride = Dual $ Endo localService
+
+    localService :: Service -> Service
+    localService svc = svc { _svcEndpoint = \_ -> localEndpoint }
+
+    localEndpoint :: Endpoint
+    localEndpoint = Endpoint {
+                      _endpointHost = "localhost"
+                    , _endpointSecure = False
+                    , _endpointPort = 8000
+                    , _endpointScope = ""
+                    }
